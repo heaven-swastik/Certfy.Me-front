3881de81fe6b63a35b77bab4a205838c31ef6e5e
Swastik Manna <swastikmanna2006@gmail.com>
2025-11-22 13:28:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppMain from './AppMain';
+import Landing from './Landing';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/app" element={<AppMain />} />
       </Routes>
     </Router>
